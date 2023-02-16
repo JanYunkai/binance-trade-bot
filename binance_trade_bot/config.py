9141888add0a9d5beb02febef58a45e7c789434e
qguduemc@163.com <qguduemc@163.com>
2023-02-16 09:31:12 +0800

    配置列表

--- binance_trade_bot/config.py
+++ binance_trade_bot/config.py
@@ -56,9 +56,10 @@ class Config:  # pylint: disable=too-few-public-methods,too-many-instance-attrib
         supported_coin_list = [
             coin.strip() for coin in os.environ.get("SUPPORTED_COIN_LIST", "").split() if coin.strip()
         ]
+        coin_list_filepath = config.get(USER_CFG_SECTION, "coin_list_filepath")
         # Get supported coin list from supported_coin_list file
-        if not supported_coin_list and os.path.exists("supported_coin_list"):
-            with open("supported_coin_list") as rfh:
+        if not supported_coin_list and os.path.exists(coin_list_filepath):
+            with open(coin_list_filepath) as rfh:
                 for line in rfh:
                     line = line.strip()
                     if not line or line.startswith("#") or line in supported_coin_list:
